72ef6e26d58c0525b37c72e4a20ca62f1424e083
Igor Burykin <iburykin071@gmail.com>
2023-11-24 13:13:06 +0200

    4.2 Blog list, step2

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 
+mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
         logger.info('connected to MongoDB')
@@ -21,8 +24,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
