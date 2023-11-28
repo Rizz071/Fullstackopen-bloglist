ab29a4836dfd272d93ac5dce29a0c71d13438def
Igor Burykin <iburykin071@gmail.com>
2023-11-28 10:59:58 +0200

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,8 +6,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, current) => sum + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let maxValue = blogs.reduce((acc, value) => {
+        return (acc = acc > value.likes ? acc : value.likes);
+    }, 0);
+
+    return blogs.find(blog => blog.likes === maxValue)
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
