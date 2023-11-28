4afab185007f776891a6d72383f3eda1748ae956
Igor Burykin <iburykin071@gmail.com>
2023-11-28 10:39:02 +0200

    4.4: helper functions and unit tests, step2

--- tests/for_testing.test.js
+++ tests/for_testing.test.js
@@ -11,6 +11,17 @@ test('dummy returns one', () => {
 describe('total likes', () => {
     const listWithOneBlog = [
         {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithBigBlog = [
+        {
             _id: "5a422a851b54a676234d17f7",
             title: "React patterns",
             author: "Michael Chan",
@@ -60,8 +71,18 @@ describe('total likes', () => {
         }
     ]
 
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+
     test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithBigBlog)
         expect(result).toBe(36)
     })
 })
