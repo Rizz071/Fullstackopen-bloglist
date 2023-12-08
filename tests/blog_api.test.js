6f2d9a2dcdbe1c3912d8fff0cb4e15f2d8b7cdc0
Igor Burykin <iburykin071@gmail.com>
2023-12-08 09:56:30 +0200

    4.14 Blog list expansions, step2

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -34,7 +34,6 @@ beforeEach(async () => {
 })
 
 
-
 describe('tests for blogs', () => {
     test('blogs are returned as json', async () => {
         await api
@@ -53,107 +52,113 @@ describe('tests for blogs', () => {
         expect(inputBlogs.body[0].id).toBeDefined()
     })
 
-    test('test for POST', async () => {
 
-        const old_Blogs_list = await api.get('/api/blogs')
+    describe('adding a blog', () => {
+        test('test for POST', async () => {
 
-        await api
-            .post('/api/blogs')
-            .send({
-                title: "test for post request 3",
-                author: "Mike Defrost",
-                url: "http://post.com",
-                likes: 3,
-            })
-            .expect(201)
+            const old_Blogs_list = await api.get('/api/blogs')
 
-        const new_Blogs_list = await api.get('/api/blogs')
-        console.log(new_Blogs_list.body)
-        expect(new_Blogs_list.body).toHaveLength(old_Blogs_list.body.length + 1)
-    })
+            await api
+                .post('/api/blogs')
+                .send({
+                    title: "test for post request 3",
+                    author: "Mike Defrost",
+                    url: "http://post.com",
+                    likes: 3,
+                })
+                .expect(201)
 
-
-    test('If the likes property is missing from the request, it will default to the value 0', async () => {
-        await api
-            .post('/api/blogs')
-            .send({
-                title: "test for post request 4",
-                author: "test for likes value",
-                url: "http://post.com",
-                // likes: 3,
-            })
-            .expect(201)
-
-        const blogList = await api.get('/api/blogs')
-        console.log(blogList.body)
-
-        blogList.body.forEach(blog => {
-            expect(blog.likes).toBeDefined()
+            const new_Blogs_list = await api.get('/api/blogs')
+            console.log(new_Blogs_list.body)
+            expect(new_Blogs_list.body).toHaveLength(old_Blogs_list.body.length + 1)
         })
 
-    })
 
-    test('If the title or url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request', async () => {
-        await api
-            .post('/api/blogs')
-            .send({
-                // title: "test for post request 5",
-                author: "test title",
-                url: "http://post.com",
-                likes: 3,
+        test('If the likes property is missing from the request, it will default to the value 0', async () => {
+            await api
+                .post('/api/blogs')
+                .send({
+                    title: "test for post request 4",
+                    author: "test for likes value",
+                    url: "http://post.com",
+                    // likes: 3,
+                })
+                .expect(201)
+
+            const blogList = await api.get('/api/blogs')
+            console.log(blogList.body)
+
+            blogList.body.forEach(blog => {
+                expect(blog.likes).toBeDefined()
             })
-            .expect(400)
+        })
 
-        await api
-            .post('/api/blogs')
-            .send({
-                title: "test for post request 5",
-                author: "test url",
-                // url: "http://post.com",
-                likes: 3,
-            })
-            .expect(400)
+        test('If the title or url properties are missing from the request data, the backend responds to the request with the status code 400 Bad Request', async () => {
+            await api
+                .post('/api/blogs')
+                .send({
+                    // title: "test for post request 5",
+                    author: "test title",
+                    url: "http://post.com",
+                    likes: 3,
+                })
+                .expect(400)
 
-        const blogList = await api.get('/api/blogs')
-        console.log(blogList.body)
-    })
+            await api
+                .post('/api/blogs')
+                .send({
+                    title: "test for post request 5",
+                    author: "test url",
+                    // url: "http://post.com",
+                    likes: 3,
+                })
+                .expect(400)
 
-    test('If entity was delete properly?', async () => {
-        const old_blogList = await api.get('/api/blogs')
-        // console.log('blogs before deletion', old_blogList.body)
-
-        await api.del(`/api/blogs/${old_blogList.body[0].id}`)
-
-        const new_blogList = await api.get('/api/blogs')
-        // console.log('blogs after deletion', new_blogList.body)
-        new_blogList.body.forEach(blog => {
-            expect(blog.id).not.toBe(old_blogList.body[0].id)
+            const blogList = await api.get('/api/blogs')
+            console.log(blogList.body)
         })
     })
 
 
-    test('If entity was updated properly?', async () => {
-        const old_blogList = await api.get('/api/blogs')
-        // console.log('first dummy in list blog before updating', old_blogList.body[0])
+    describe('deletion of a blog', () => {
+        test('If entity was delete properly?', async () => {
+            const old_blogList = await api.get('/api/blogs')
+            // console.log('blogs before deletion', old_blogList.body)
 
-        //trying to update first dummy blog in list. Likes property is different
-        const updatedBlog = {
-            title: 'test for post request 0',
-            author: 'John Post',
-            url: 'http://post.com',
-            likes: 100,
-            id: old_blogList.body[0].id,
-        }
+            await api.del(`/api/blogs/${old_blogList.body[0].id}`)
 
-        await api
-            .put(`/api/blogs/${old_blogList.body[0].id}`)
-            .send(updatedBlog)
-            .expect(200)
+            const new_blogList = await api.get('/api/blogs')
+            // console.log('blogs after deletion', new_blogList.body)
+            new_blogList.body.forEach(blog => {
+                expect(blog.id).not.toBe(old_blogList.body[0].id)
+            })
+        })
+    })
 
-        const new_blogList = await api.get('/api/blogs')
-        // console.log('dummy blog after updating', new_blogList.body[0])
+    describe('updating of a blog', () => {
+        test('If entity was updated properly?', async () => {
+            const old_blogList = await api.get('/api/blogs')
+            // console.log('first dummy in list blog before updating', old_blogList.body[0])
 
-        expect(new_blogList.body[0]).toEqual(updatedBlog)
+            //trying to update first dummy blog in list. Likes property is different
+            const updatedBlog = {
+                title: 'test for post request 0',
+                author: 'John Post',
+                url: 'http://post.com',
+                likes: 100,
+                id: old_blogList.body[0].id,
+            }
+
+            await api
+                .put(`/api/blogs/${old_blogList.body[0].id}`)
+                .send(updatedBlog)
+                .expect(200)
+
+            const new_blogList = await api.get('/api/blogs')
+            // console.log('dummy blog after updating', new_blogList.body[0])
+
+            expect(new_blogList.body[0]).toEqual(updatedBlog)
+        })
     })
 
     afterAll(async () => {
